cd13fcb465478d8443366147cd035f27da178c5f
Derrak Richard <derrak@gmail.com>
2022-10-02 09:09:08 -0700

    add hooks to react example

--- react-d3-example1/src/App.js
+++ react-d3-example1/src/App.js
@@ -1,22 +1,11 @@
 import React, {Component} from 'react';
-import Test from './Test';
+import ChartWrapper from './ChartWrapper';
 
 class App extends Component {
-  state = {
-    personClicks: 0
-  }
-
-  personClicked = () => {
-    this.setState({personClicks: this.state.personClicks + 1})
-  }
-
   render() {
     return (
       <div className="App">
-        <Test person="Derrak" personClicked={this.personClicked}/>
-        <Test person="Laurel" personClicked={this.personClicked}/>
-        <Test person="William" personClicked={this.personClicked}/>
-        Person Clicks: {this.state.personClicks}
+    <ChartWrapper />
       </div>
   );
     }
