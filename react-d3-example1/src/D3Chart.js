6b2215379d548979d97a748a16f993deb7f216a9
Derrak Richard <derrak@gmail.com>
2022-10-02 11:37:03 -0700

    connect firebase data

--- react-d3-example1/src/D3Chart.js
+++ react-d3-example1/src/D3Chart.js
@@ -1,5 +1,8 @@
 import * as d3 from 'd3';
 
+const data = [];
+const url = "https://udemy-react-d3.firebaseio.com/ages.json"
+
 export default class D3Chart {
   constructor(element){
     const svg = d3.select(element)
@@ -7,7 +10,15 @@ export default class D3Chart {
       .attr('width', 500)
       .attr('height', 500)
 
-    svg.append('rect')
+    d3.json(url).then(agesData => {
+      console.log(agesData);
+    })
+
+    const rects = svg.selectAll('rects')
+      .data(data)
+
+    rects.enter()
+      .append('rect')
       .attr('x', 50)
       .attr('y', 50)
       .attr('width', 100)
