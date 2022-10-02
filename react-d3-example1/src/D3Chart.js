d34712027c7e4646bf4213d9c10f557e4ed99129
Derrak Richard <derrak@gmail.com>
2022-10-02 12:11:52 -0700

    bar chart style updates

--- react-d3-example1/src/D3Chart.js
+++ react-d3-example1/src/D3Chart.js
@@ -1,28 +1,30 @@
 import * as d3 from 'd3';
 
-const data = [];
 const url = "https://udemy-react-d3.firebaseio.com/ages.json"
 
 export default class D3Chart {
   constructor(element){
     const svg = d3.select(element)
       .append('svg')
-      .attr('width', 500)
-      .attr('height', 500)
+        .attr('width', 500)
+        .attr('height', 500)
 
     d3.json(url).then(agesData => {
-      console.log(agesData);
+      const rects = svg.selectAll('rect')
+        .data(agesData)
+      
+      rects.enter()
+        .append('rect')
+          .attr('x', (d, i) => i * 100)
+          .attr('y', 50)
+          .attr('width', 50)
+          .attr('height', d => d.age * 10)
+          .attr('fill', d => {
+            if (d.name == "Tony") {
+              return 'red'
+            }
+            else {return 'gray'}
+          })
     })
-
-    const rects = svg.selectAll('rects')
-      .data(data)
-
-    rects.enter()
-      .append('rect')
-      .attr('x', 50)
-      .attr('y', 50)
-      .attr('width', 100)
-      .attr('height', 400)
-      .attr('fill', 'grey')
   }
 }
