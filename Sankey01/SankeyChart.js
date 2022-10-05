662685ab2a3cd3d3d06e6c478648771903a69828
Derrak Richard <derrak@gmail.com>
2022-10-05 13:06:34 -0700

    add sankey to Index

--- Sankey01/SankeyChart.js
+++ Sankey01/SankeyChart.js
@@ -1,93 +1,111 @@
+import { sankey } from 'd3-sankey';
+// sankey = require('d3-sankey');
+
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
-    width = 450 - margin.left - margin.right,
-    height = 480 - margin.top - margin.bottom;
+    width = 900 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;  
 
+// format variables
+var formatNumber = d3.format(",.0f"), // zero decimal places
+    format = function(d) { return formatNumber(d); },
+    color = d3.scaleOrdinal(d3.schemeCategory10);
+  
 // append the svg object to the body of the page
-var svg = d3.select(".canvas").append("svg")
+var svg = d3.select("#graph").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
+    .attr("transform", 
           "translate(" + margin.left + "," + margin.top + ")");
-
-// Color scale used
-var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 // Set the sankey diagram properties
 var sankey = d3.sankey()
     .nodeWidth(36)
-    .nodePadding(290)
+    .nodePadding(40)
     .size([width, height]);
 
+var path = sankey.links();
+
 // load the data
-d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_sankey.json", function(error, graph) {
+d3.csv("SankeyData.csv").then(function(data) {
 
-  // Constructs a new Sankey generator with the default settings.
-  sankey
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .layout(1);
+  //set up graph in same style as original example but empty
+  sankeydata = {"nodes" : [], "links" : []};
 
-  // add in the links
-  var link = svg.append("g")
-    .selectAll(".link")
-    .data(graph.links)
-    .enter()
-    .append("path")
+  data.forEach(function (d) {
+    sankeydata.nodes.push({ "name": d.source });
+    sankeydata.nodes.push({ "name": d.target });
+    sankeydata.links.push({ "source": d.source,
+                       "target": d.target,
+                       "value": +d.value });
+   });
+
+  // return only the distinct / unique nodes
+ sankeydata.nodes = Array.from(
+    d3.group(sankeydata.nodes, d => d.name),
+	([value]) => (value)
+  );
+
+  // loop through each link replacing the text with its index from node
+  sankeydata.links.forEach(function (d, i) {
+    sankeydata.links[i].source = sankeydata.nodes
+      .indexOf(sankeydata.links[i].source);
+    sankeydata.links[i].target = sankeydata.nodes
+      .indexOf(sankeydata.links[i].target);
+  });
+
+  // now loop through each nodes to make nodes an array of objects
+  // rather than an array of strings
+  sankeydata.nodes.forEach(function (d, i) {
+    sankeydata.nodes[i] = { "name": d };
+  });
+
+  graph = sankey(sankeydata);
+
+// add in the links
+  var link = svg.append("g").selectAll(".link")
+      .data(graph.links)
+    .enter().append("path")
       .attr("class", "link")
-      .attr("d", sankey.link() )
-      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-      .sort(function(a, b) { return b.dy - a.dy; });
+      .attr("d", d3.sankeyLinkHorizontal())
+      .attr("stroke-width", function(d) { return d.width; });  
 
-  // add in the nodes
-  var node = svg.append("g")
-    .selectAll(".node")
-    .data(graph.nodes)
+// add the link titles
+  link.append("title")
+        .text(function(d) {
+    		    return d.source.name + " → " + 
+                d.target.name + "\n" + format(d.value); });
+
+// add in the nodes
+  var node = svg.append("g").selectAll(".node")
+      .data(graph.nodes)
     .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .call(d3.drag()
-        .subject(function(d) { return d; })
-        .on("start", function() { this.parentNode.appendChild(this); })
-        .on("drag", dragmove));
+      .attr("class", "node");
 
-  // add the rectangles for the nodes
-  node
-    .append("rect")
-      .attr("height", function(d) { return d.dy; })
+// add the rectangles for the nodes
+  node.append("rect")
+      .attr("x", function(d) { return d.x0; })
+      .attr("y", function(d) { return d.y0; })
+      .attr("height", function(d) { return d.y1 - d.y0; })
       .attr("width", sankey.nodeWidth())
-      .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
-      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
-    // Add hover text
-    .append("title")
-      .text(function(d) { return d.name + "\n" + "There is " + d.value + " stuff in this node"; });
+      .style("fill", function(d) { 
+		      return d.color = color(d.name.replace(/ .*/, "")); })
+      .style("stroke", function(d) { 
+		  return d3.rgb(d.color).darker(2); })
+	  .append("title")
+      .text(function(d) { 
+		  return d.name + "\n" + format(d.value); });
 
-  // add in the title for the nodes
-    node
-      .append("text")
-        .attr("x", -6)
-        .attr("y", function(d) { return d.dy / 2; })
-        .attr("dy", ".35em")
-        .attr("text-anchor", "end")
-        .attr("transform", null)
-        .text(function(d) { return d.name; })
-      .filter(function(d) { return d.x < width / 2; })
-        .attr("x", 6 + sankey.nodeWidth())
-        .attr("text-anchor", "start");
-
-  // the function for moving the nodes
-  function dragmove(d) {
-    d3.select(this)
-      .attr("transform",
-            "translate("
-               + d.x + ","
-               + (d.y = Math.max(
-                  0, Math.min(height - d.dy, d3.event.y))
-                 ) + ")");
-    sankey.relayout();
-    link.attr("d", sankey.link() );
-  }
+// add in the title for the nodes
+  node.append("text")
+      .attr("x", function(d) { return d.x0 - 6; })
+      .attr("y", function(d) { return (d.y1 + d.y0) / 2; })
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "end")
+      .text(function(d) { return d.name; })
+    .filter(function(d) { return d.x0 < width / 2; })
+      .attr("x", function(d) { return d.x1 + 6; })
+      .attr("text-anchor", "start");
 
 });
-
